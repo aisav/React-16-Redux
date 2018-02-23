66eaeae19df0b7cb9e5fa1273b6ba7907eb09096
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-02-23 11:56:54 -0800

    anoyher component method call & style

--- src/App.js
+++ src/App.js
@@ -12,25 +12,39 @@ class App extends Component {
         another: 'hhh'
     }
 
-    handleSwitchName = () => {
+    handleSwitchName = (newName) => {
         this.setState({persons: [
-            {name: 'Isaverdyan', age: 31},
+            {name: newName, age: 31},
             {name: 'Tom', age: 30}
         ]})
     }
+
+    handleChangeName = (event) => {
+        this.setState({persons: [
+            {name: 'notChanged', age: 11},
+            {name: event.target.value, age: 11}
+        ]})
+    }
   render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px'
+        };
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" style={style}/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
             Hello, I am a React app
-            <button onClick={this.handleSwitchName}>Switch Name</button>
+            <button onClick={() => this.handleSwitchName('Isaverdyan')}>Switch Name</button>
         </p>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies: Walking</Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+                  clicked={this.handleSwitchName.bind(this, 'Isav!!!!!!!!')} changed = {this.handleChangeName}/>
       </div>
     );
     // return React.createElement('h1', null, 'kkkkkkkkkk');
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Person.css';
 
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             {/*<p>I am a person and I am {Math.floor(Math.random()*30)} years old</p>*/}
-            <p>I am {props.name} and I am {props.age} years old</p>
+            <p onClick={props.clicked}>I am {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
 )
 };
