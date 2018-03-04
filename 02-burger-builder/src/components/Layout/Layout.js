95bb0752cae03d3b9de626ddeb0e404823d34e61
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-03-04 09:20:54 -0800

    with static BurgerIngredients

--- 02-burger-builder/src/components/Layout/Layout.js
+++ 02-burger-builder/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
+
 import Auxx from '../../hoc/Auxx';
 import classes from './Layout.css';
 
-const layout = (props) => (
+const layout = ( props ) => (
     <Auxx>
-        <div>Toolbar, SideDrawer, BackDrop</div>
+        <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
--- 02-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 02-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+
 import Auxx from '../../hoc/Auxx';
 import Burger from '../../components/Burger/Burger';
 
-
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render () {
         return (
             <Auxx>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Auxx>
-        )
+        );
     }
 }
 
