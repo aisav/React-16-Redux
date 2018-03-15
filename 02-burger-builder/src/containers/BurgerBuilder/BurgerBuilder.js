8072f1ad1ebae746d9280b05766b636354e46179
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-03-15 15:36:47 -0700

    buildcontrols, ingredients added dynamicly

--- 02-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 02-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 import Auxx from '../../hoc/Auxx';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+        salad: 0.5,
+        bacon: 0.6,
+        cheese: 0.7,
+        meat: 1
+}
 
 class BurgerBuilder extends Component {
     // constructor(props) {
@@ -10,18 +18,32 @@ class BurgerBuilder extends Component {
     // }
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 3,
-            meat: 2
-        }
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 11
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount +1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type]
+        const oldPrice = this.state.ingredients.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
     }
 
     render () {
         return (
             <Auxx>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls ingredientAdded={this.addIngredientHandler}/>
             </Auxx>
         );
     }
