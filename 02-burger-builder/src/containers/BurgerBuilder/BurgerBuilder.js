77042ce8a6077debad7020ec6db35ef80c793446
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-03-17 10:33:58 -0700

    disable atribute for Less button

--- 02-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 02-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -56,12 +56,18 @@ class BurgerBuilder extends Component {
     }
 
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo)
+            disabledInfo[key] = disabledInfo[key] <= 0;
         return (
             <Auxx>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}/>
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled = {disabledInfo}/>
             </Auxx>
         );
     }
