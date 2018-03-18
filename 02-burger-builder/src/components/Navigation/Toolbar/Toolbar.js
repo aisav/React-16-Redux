b07c3bc0faa373ed8c64a1174a4c4215d04d7865
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-03-18 11:37:15 -0700

    navigation items

--- 02-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ 02-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
@@ -9,7 +10,7 @@ const toolbar = (props) => (
         <Logo/>
         <nav>
             <ul>
-                ...
+                <NavigationItems/>
             </ul>
         </nav>
     </header>
