eb3476762f7957f4450d1442be956b67ce7b5445
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-03-24 08:01:14 -0700

    responsive design & Slidedrawer

--- 02-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ 02-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -7,11 +7,11 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
-        <Logo/>
-        <nav>
-            <ul>
+        <div className={classes.Logo}>
+            <Logo/>
+        </div>
+        <nav className={classes.DesktopOnly}>
                 <NavigationItems/>
-            </ul>
         </nav>
     </header>
 
