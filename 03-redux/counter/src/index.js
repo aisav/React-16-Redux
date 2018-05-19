c3fc74aadf76cade5f269a1038520779cc08e7af
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-05-19 21:31:05 +0400

    devtools & first middleware

--- 03-redux/counter/src/index.js
+++ 03-redux/counter/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -14,8 +14,26 @@ var rootReducer = combineReducers({
     res: resultReducer
 });
 
+//middleware
+
+const logger = (store) => {
+    return (next) => {
+        return (action) => {
+            console.log('[Middleware] dispatching' + JSON.stringify(action));
+            const result = next(action);
+            console.log('[Middleware] next state' + JSON.stringify(store.getState()));
+            return result;
+        }
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
 //redux store creation
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
