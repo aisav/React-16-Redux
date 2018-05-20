c53d7a94fd4c076ee7a328799f74ff99ac32ea6b
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-05-20 08:09:15 +0400

    fixing results key

--- 03-redux/counter/src/store/reducers/result.js
+++ 03-redux/counter/src/store/reducers/result.js
@@ -9,7 +9,7 @@ const reducer = (state = initialState, action) => {
         case actionTypes.STORE_RESULT:
             return {
                 ...state,
-                results: state.results.concat({id: new Date(), val: action.result })
+                results: state.results.concat({id: new Date().getTime(), val: action.result })
             }
         case actionTypes.DELETE_RESULT:
             return {
