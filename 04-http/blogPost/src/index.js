6f044635cf233f2eb541edfee65aaa141c10c1b4
aisav <artur.isaverdyan@gmail.com>
2018-05-25 06:30:00 -0700

    interseptors axios

--- 04-http/blogPost/src/index.js
+++ 04-http/blogPost/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
+axios.interceptors.request.use(requestConfig => {
+    console.log("request"+requestConfig)
+    return requestConfig;
+}, error => {
+    console.log("error"+error)
+    // return Promise.resolve(error);
+    return Promise.reject(error);
+})
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
