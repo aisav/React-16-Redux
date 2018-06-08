f61943c8fdc135facb1380fa9a8a4edc3aa37b99
aisav <artur.isaverdyan@gmail.com>
2018-06-09 00:44:57 +0500

    orders fetching and outputing

--- 02-burger-builder/src/App.js
+++ 02-burger-builder/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { Route, Switch, BrowserRouter  } from 'react-router-dom';
+
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   render() {
     return (
-      <div >
+      <BrowserRouter  >
         <Layout>
-          <BurgerBuilder/>
+            <Switch>
+                <Route path="/orders" component={Orders} />
+                <Route path="/" exact component={BurgerBuilder} />
+            </Switch>
         </Layout>
-      </div>
+      </BrowserRouter>
     );
   }
 }
