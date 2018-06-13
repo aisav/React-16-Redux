cd6ac0623ce3af51d1308d9b82207946748228ca
aisav <artur.isaverdyan@gmail.com>
2018-06-13 20:10:49 +0400

    mobile side

--- 02-burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ 02-burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -2,8 +2,10 @@ import React from 'react';
 import classes from './DrawerToggle.css';
 
 const drawerToggle = (props) => (
-    <div onClick={props.clicked} >
-        MENU FOR MOBILE DEVICES
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 )
 
