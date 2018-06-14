e6d8ce5a0126c1b7f9a0c1ee21cf0ec38d256982
aisav <artur.isaverdyan@gmail.com>
2018-06-14 20:50:39 +0400

    reducer test

---
+++ 02-burger-builder/src/store/reducers/burgerBuilder.test.js
@@ -0,0 +1,65 @@
+import * as actionTypes from '../actions/actionTypes';
+import reducer from './burgerBuilder';
+
+
+describe('========reducer========', () => {
+    it('ADD_INGREDIENT', () => {
+        expect(reducer({
+                    ingredients: {salad: 0},
+                    totalPrice: 5
+            },
+            {
+                type: actionTypes.ADD_INGREDIENT,
+                ingredientName: 'salad'
+            }
+        )).toEqual({
+                ingredients: {'salad': 1},
+                totalPrice: 5.5,
+        });
+    });
+
+    it('REMOVE_INGREDIENT', () => {
+        expect(reducer({
+                ingredients: {
+                    salad: 1,
+                    bacon: 0,
+                    cheese: 1,
+                    meat: 0
+                },
+                totalPrice: 5.9
+            },
+            {
+                type: actionTypes.REMOVE_INGREDIENT,
+                ingredientName: 'cheese'
+
+            }
+
+        )).toEqual({
+            ingredients: {
+                salad: 1,
+                bacon: 0,
+                cheese: 0,
+                meat: 0
+            },
+            totalPrice: 5.5
+        });
+    });
+
+    it('SET_INGREDIENTS', () => {
+        expect(reducer({
+                ingredients: {salad: 0},
+                totalPrice: 5
+            },
+            {
+                type: actionTypes.ADD_INGREDIENT,
+                ingredientName: 'salad'
+            }
+        )).toEqual({
+            ingredients: {'salad': 1},
+            totalPrice: 5.5,
+        });
+    });
+
+});
+
+
