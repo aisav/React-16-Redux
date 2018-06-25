7a689ea0a9271ca65c993c4063e553f0909f87b9
aisav <artur.isaverdyan@gmail.com>
2018-06-26 03:40:50 +0500

    ui

--- 04-http/blogPost/src/containers/Blog/NewMessage/NewMessage.js
+++ 04-http/blogPost/src/containers/Blog/NewMessage/NewMessage.js
@@ -7,52 +7,52 @@ import TextField from 'material-ui/TextField'
 class NewMessage extends Component {
     state = {
         name: '',
-        email: '',
         body: '',
     }
 
-    postDataHandler = () => {
+    postDataHandler = (e) => {
+        e.preventDefault();
         const post = {
             name: this.state.name,
-            body: this.state.body,
-            email: this.state.email
+            body: this.state.body
         }
         this.props.onNewMessage(post)
 
     }
 
     render() {
-
+        const {name, email, body} = this.state
         return (
-            <div>
+            <form onSubmit={this.postDataHandler}>
                 <h3>Create a New Message</h3>
                 <TextField
                     id="name"
                     label="Name"
-                    value={this.state.name}
+                    value={name}
                     onChange={(event) => this.setState({name: event.target.value})}
                     margin="normal"
                     fullWidth
+                    required
                 /><br/>
                 <TextField
                     id="multiline"
                     label="Body"
                     multiline
                     rowsMax="4"
-                    value={this.state.body}
+                    value={body}
                     onChange={(event) => this.setState({body: event.target.value})}
                     fullWidth
                     margin="normal"
+                    required
                 /><br/>
                 <Button
                     type='submit'
                     color='primary'
-                    onClick={() => this.postDataHandler()}
                     variant='raised'
                 >
                     Create
                 </Button>
-            </div>
+            </form>
         )
             ;
     }
