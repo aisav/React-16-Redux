3c70308734cc86fb84f8ab4f05f60e91d86f2c69
aisav <artur.isaverdyan@gmail.com>
2018-06-28 15:59:16 +0500

    quantityById impl in the store

--- 05-online-web-store/src/store/reducers/cart.js
+++ 05-online-web-store/src/store/reducers/cart.js
@@ -1,22 +1,35 @@
 import * as actionTypes from '../actions/actionTypes'
 
 const initialState = {
-    products: []
+    products: [],
+    quantityById: {}
 };
 
 const reducer = (state = initialState, action) => {
+    let index = state.products.indexOf(action.payload) //find current product in our pr. array
+    let helpObj = {}
+    let count
+
     switch (action.type) {
         case actionTypes.ADD:
+            count = ((index === -1) ? 1 :  state.quantityById[action.payload.id] + 1)
+            helpObj[action.payload.id]=count
             return {
                 ...state,
-                products: state.products.concat(action.payload)
+                products: state.products.concat(action.payload),
+                quantityById: {...state.quantityById, ...helpObj}
             }
 
         case actionTypes.REMOVE:
-            let index = state.products.indexOf(action.payload)
+            if(index!==-1){
+                count = state.quantityById[action.payload.id] - 1
+                helpObj[action.payload.id]=count
+            }
+
             return {
                 ...state,
-                products: state.products.filter((item, id) => id !== index)
+                products: state.products.filter((item, id) => id !== index),
+                quantityById: {...state.quantityById, ...helpObj}
             }
         default:
             return state;
